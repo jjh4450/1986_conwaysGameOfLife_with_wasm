7e7e48d64561d5f2874e92e7aa9d6552343b65bd
JunHWan Jo (jeje) <jjh4450git@gamil.com>
2024-04-13 15:23:26 +0900

    first commit

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite';
+import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
+import path from 'path';
+
+export default defineConfig({
+  root: path.resolve(__dirname, 'src'),
+  server: {
+    port: 8080,
+    hot: true
+  },
+
+  build: {
+    outDir: '../dist'
+  },
+
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  },
+
+  plugins: [
+    wasm(),
+    topLevelAwait()
+  ],
+
+  optimizeDeps: {
+    exclude: [
+      '@syntect/wasm'
+    ]
+  }
+});
