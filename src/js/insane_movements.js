0297fd882f916ef8f467570079adc4aed0e82d26
JunHWan Jo (jeje) <jjh4450git@gamil.com>
2024-04-14 14:23:59 +0900

    delete code

--- src/js/insane_movements.js
+++ src/js/insane_movements.js
@@ -1,7 +1,6 @@
 export default function title_render(){
     const xMax = window.innerWidth - 100; // iframe width
     const yMax = window.innerHeight - 100; // iframe height
-    console.log(xMax, yMax);
     const xPos = Math.floor(Math.random() * xMax);
     const yPos = Math.floor(Math.random() * yMax);
     document.documentElement.style.setProperty('--x-pos', `${xPos}px`);
